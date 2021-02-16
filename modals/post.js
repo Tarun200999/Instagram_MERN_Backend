710927783005f533a652f8fe67e63dd715e3b674
tarun <tk4977163@gmail.com>
2021-02-16 17:11:59 +0530

    some minor changes in code

--- modals/post.js
+++ modals/post.js
@@ -1,22 +1,22 @@
-const mongoose=require("mongoose");
-const {ObjectId}=mongoose.Schema.Types;
-const newPostSchema=new mongoose.Schema({
-    title:{
-        type:String,
-        required:true,
-    },
-    body:{
-        type:String,
-        required:true,
-    },
-    photo:{
-        type:String,
-        default:"no photo",
-    },
-    postBy:{
-        type:ObjectId,
-        ref:"Users",
-    }
-})
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
+const newPostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  postBy: {
+    type: ObjectId,
+    ref: "Users",
+  },
+});
 
-mongoose.model("Posts",newPostSchema);
+mongoose.model("Posts", newPostSchema);
--- modals/user.js
+++ modals/user.js
@@ -1,20 +1,20 @@
 //USER SCHEAMA IS HERE
 
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const userSchema=new mongoose.Schema({
-    name:{
-      type:String,
-      required:true,
-    },
-    email:{
-        type:String,
-        required:true,
-    },
-    password:{
-        type:String,
-        required:true,
-    }
-})
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-mongoose.model("Users",userSchema);
+mongoose.model("Users", userSchema);
